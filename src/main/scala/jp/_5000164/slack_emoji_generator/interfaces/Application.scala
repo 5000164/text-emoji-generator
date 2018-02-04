8bf9fed6641f4d32fba7c69f0348f8ebfe1a8797
菅原 浩 <sncts35326@gmail.com>
2018-02-04 20:26:43 +0900

    feat: 生成ボタンを押した時に Canvas に内容を描画するようにし、保存ボタンを押した時に保存処理を行うようにする

--- src/main/scala/jp/_5000164/slack_emoji_generator/interfaces/Application.scala
+++ src/main/scala/jp/_5000164/slack_emoji_generator/interfaces/Application.scala
@@ -13,12 +13,23 @@ import scala.scalajs.js.DynamicImplicits._
 import scalacss.ScalaCssReact._
 
 object Application extends App {
-  val canvas = ScalaComponent.builder[Unit]("Canvas")
-    .renderStatic(<.canvas(Styles.canvas, ^.id := "canvas"))
+  val content = ScalaComponent.builder[Unit]("content")
+    .renderStatic(<.div(
+      <.div(
+        <.canvas(^.id := "canvas", Styles.canvas)
+      ),
+      <.div(
+        <.input(),
+        <.button(^.onClick --> generate, "生成")
+      ),
+      <.div(
+        <.button(^.onClick --> save, "保存")
+      )
+    ))
     .build
 
   Styles.addToDocument()
-  canvas().renderIntoDOM(document.getElementById("root"))
+  content().renderIntoDOM(document.getElementById("root"))
 
   val c = document.getElementById("canvas").asInstanceOf[Canvas]
   c.width = 128
@@ -28,20 +39,26 @@ object Application extends App {
   ctx.font = "bold 64px 'Hiragino Kaku Gothic Pro'"
   ctx.textAlign = "center"
   ctx.textBaseline = "middle"
-  ctx.fillText("あ", 32, 32)
-  ctx.fillText("あ", 96, 32)
-  ctx.fillText("あ", 32, 96)
-  ctx.fillText("あ", 96, 96)
 
-  val dialog = js.Dynamic.global.require("electron").remote.dialog
-  val option = js.Dynamic.literal("defaultPath" -> "emoji.png")
-  val callback = (x: String) => {
-    val image = c.toDataURL("image/png").drop("data:image/png;base64,".length)
-    val fs = js.Dynamic.global.require("fs")
-    fs.writeFile(x, image, js.Dynamic.literal("encoding" -> "base64"), { (err: js.Dynamic) =>
-      if (err) println(err)
-      else println("saved")
-    })
+  def generate = Callback {
+    ctx.fillText("あ", 32, 32)
+    ctx.fillText("あ", 96, 32)
+    ctx.fillText("あ", 32, 96)
+    ctx.fillText("あ", 96, 96)
+
   }
-  dialog.showSaveDialog(null, option, callback)
+
+  def save = Callback {
+    val dialog = js.Dynamic.global.require("electron").remote.dialog
+    val option = js.Dynamic.literal("defaultPath" -> "emoji.png")
+    val callback = (x: String) => {
+      val image = c.toDataURL("image/png").drop("data:image/png;base64,".length)
+      val fs = js.Dynamic.global.require("fs")
+      fs.writeFile(x, image, js.Dynamic.literal("encoding" -> "base64"), { (err: js.Dynamic) =>
+        if (err) println(err)
+        else println("saved")
+      })
+    }
+    dialog.showSaveDialog(null, option, callback)
+  }
 }
