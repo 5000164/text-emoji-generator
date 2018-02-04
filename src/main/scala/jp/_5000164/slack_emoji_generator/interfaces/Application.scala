c173af8bb431d0a22ed6337bbb22176eb688fa93
菅原 浩 <sncts35326@gmail.com>
2018-02-04 21:00:22 +0900

    feat: 入力したテキストを Canvas に描画する

--- src/main/scala/jp/_5000164/slack_emoji_generator/interfaces/Application.scala
+++ src/main/scala/jp/_5000164/slack_emoji_generator/interfaces/Application.scala
@@ -14,41 +14,53 @@ import scalacss.ScalaCssReact._
 
 object Application extends App {
   val content = ScalaComponent.builder[Unit]("content")
-    .renderStatic(<.div(
-      <.div(
-        <.canvas(^.id := "canvas", Styles.canvas)
-      ),
-      <.div(
-        <.input(),
-        <.button(^.onClick --> generate, "生成")
-      ),
-      <.div(
-        <.button(^.onClick --> save, "保存")
-      )
-    ))
+    .initialState(State(""))
+    .renderBackend[Backend]
     .build
 
   Styles.addToDocument()
   content().renderIntoDOM(document.getElementById("root"))
+}
 
-  val c = document.getElementById("canvas").asInstanceOf[Canvas]
-  c.width = 128
-  c.height = 128
-  type Ctx2D = dom.CanvasRenderingContext2D
-  val ctx = c.getContext("2d").asInstanceOf[Ctx2D]
-  ctx.font = "bold 64px 'Hiragino Kaku Gothic Pro'"
-  ctx.textAlign = "center"
-  ctx.textBaseline = "middle"
+case class State(text: String)
 
-  def generate = Callback {
-    ctx.fillText("あ", 32, 32)
-    ctx.fillText("あ", 96, 32)
-    ctx.fillText("あ", 32, 96)
-    ctx.fillText("あ", 96, 96)
+class Backend($: BackendScope[Unit, State]) {
+  def render(state: State) = <.div(
+    <.div(
+      <.canvas(^.id := "canvas", Styles.canvas)
+    ),
+    <.div(
+      <.input(^.value := state.text, ^.onChange ==> onChange),
+      <.button(^.onClick --> generate(state.text), "生成")
+    ),
+    <.div(
+      <.button(^.onClick --> save, "保存")
+    )
+  )
 
+  def onChange(e: ReactEventFromInput): CallbackTo[Unit] = {
+    val newValue = e.target.value
+    $.modState(_.copy(text = newValue))
+  }
+
+  def generate(text: String) = Callback {
+    val c = document.getElementById("canvas").asInstanceOf[Canvas]
+    c.width = 128
+    c.height = 128
+    type Ctx2D = dom.CanvasRenderingContext2D
+    val ctx = c.getContext("2d").asInstanceOf[Ctx2D]
+    ctx.font = "bold 64px 'Hiragino Kaku Gothic Pro'"
+    ctx.textAlign = "center"
+    ctx.textBaseline = "middle"
+
+    ctx.fillText(text.charAt(0).toString, 32, 32)
+    ctx.fillText(text.charAt(1).toString, 96, 32)
+    ctx.fillText(text.charAt(2).toString, 32, 96)
+    ctx.fillText(text.charAt(3).toString, 96, 96)
   }
 
   def save = Callback {
+    val c = document.getElementById("canvas").asInstanceOf[Canvas]
     val dialog = js.Dynamic.global.require("electron").remote.dialog
     val option = js.Dynamic.literal("defaultPath" -> "emoji.png")
     val callback = (x: String) => {
