13c3d1279f031580f69eae1bd408c3e58c14c486
Hiroshi Sugawara <5000164@users.noreply.github.com>
2018-02-04 18:33:22 +0900

    feat: canvas に文字を表示

--- src/main/scala/jp/_5000164/slack_emoji_generator/interfaces/Application.scala
+++ src/main/scala/jp/_5000164/slack_emoji_generator/interfaces/Application.scala
@@ -4,7 +4,9 @@ import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
 import jp._5000164.slack_emoji_generator.CssSettings._
 import jp._5000164.slack_emoji_generator.Styles
+import org.scalajs.dom
 import org.scalajs.dom.document
+import org.scalajs.dom.html.Canvas
 
 import scalacss.ScalaCssReact._
 
@@ -15,4 +17,17 @@ object Application extends App {
 
   Styles.addToDocument()
   canvas().renderIntoDOM(document.getElementById("root"))
+
+  val c = document.getElementById("canvas").asInstanceOf[Canvas]
+  c.width = 128
+  c.height = 128
+  type Ctx2D = dom.CanvasRenderingContext2D
+  val ctx = c.getContext("2d").asInstanceOf[Ctx2D]
+  ctx.font = "bold 64px 'Hiragino Kaku Gothic Pro'"
+  ctx.textAlign = "center"
+  ctx.textBaseline = "middle"
+  ctx.fillText("あ", 32, 32)
+  ctx.fillText("あ", 96, 32)
+  ctx.fillText("あ", 32, 96)
+  ctx.fillText("あ", 96, 96)
 }
