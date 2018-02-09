88fc5cc7c4243d4f29d688829fc4917dac6fe6fa
菅原 浩 <sncts35326@gmail.com>
2018-02-09 22:55:23 +0900

    feat: 色を自分で設定できるようにする

--- src/main/scala/jp/_5000164/slack_emoji_generator/interfaces/Backend.scala
+++ src/main/scala/jp/_5000164/slack_emoji_generator/interfaces/Backend.scala
@@ -1,20 +1,24 @@
 package jp._5000164.slack_emoji_generator.interfaces
 
-import japgolly.scalajs.react.vdom.html_<^._
-import japgolly.scalajs.react.{BackendScope, Callback, ReactEventFromInput}
+import japgolly.scalajs.react.vdom.html_<^.{<, _}
+import japgolly.scalajs.react.{BackendScope, Callback, ReactEventFromInput, StateAccessPure}
 import jp._5000164.slack_emoji_generator.domain.State
+import jp._5000164.slack_emoji_generator.interfaces.Canvas.colorList
 
 import scala.scalajs.js
+import scala.util.Random
 import scalacss.ScalaCssReact._
 
 class Backend($: BackendScope[Unit, State]) {
   def render(state: State): VdomElement = {
+    val f = $.zoomState(_.color)(value => _.copy(color = value))
     <.div(
       <.div(
         <.canvas(^.id := "canvas", Styles.canvas)
       ),
       <.div(
-        <.textarea(^.value := state.text, ^.onChange ==> onChange)
+        <.textarea(^.value := state.text, ^.onChange ==> onChangeText(f)),
+        <.input(^.value := state.color, ^.onChange ==> onChangeColor(state.text))
       ),
       <.div(
         <.button(^.onClick --> Canvas.save(state.text), "保存")
@@ -23,7 +27,7 @@ class Backend($: BackendScope[Unit, State]) {
         <.ul(Canvas.colorList.toVdomArray({
           case (key, value) => <.li(
             ^.key := key,
-            ^.onClick --> Canvas.generateWithColor(state.text, value),
+            ^.onClick --> onClickColor(state.text, value, f),
             ^.style := js.Dictionary("backgroundColor" -> s"#$value").asInstanceOf[js.Object],
             Styles.colorItem
           )
@@ -32,11 +36,30 @@ class Backend($: BackendScope[Unit, State]) {
     )
   }
 
-  def onChange(e: ReactEventFromInput): Callback = {
+  def onChangeText(s: StateAccessPure[String])(e: ReactEventFromInput): Callback = {
     val updatedText = e.target.value
-    $.modState(_.copy(text = updatedText))
+    val color = Random.shuffle(colorList.values).head
+
+    {
+      $.modState(_.copy(text = updatedText))
+    } >> {
+      Canvas.generate(updatedText, color)
+    } >> {
+      s.setState(color)
+    }
+  }
+
+  def onChangeColor(text: String)(e: ReactEventFromInput): Callback = {
+    val updatedColor = e.target.value
+    $.modState(_.copy(color = updatedColor))
   } >> {
-    val updatedText = e.target.value
-    Canvas.generate(updatedText)
+    val updatedColor = e.target.value
+    Canvas.generate(text, updatedColor)
+  }
+
+  def onClickColor(text: String, color: String, s: StateAccessPure[String]): Callback = {
+    Canvas.generate(text, color)
+  } >> {
+    s.setState(color)
   }
 }
