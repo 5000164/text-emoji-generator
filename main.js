62b0d2d925479f3409d6b00501093f4c864f85bf
Hiroshi Sugawara <5000164@users.noreply.github.com>
2018-02-10 19:50:08 +0900

    feat: ウィンドウサイズの初期値を小さくする

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const url = require('url')
 let win
 
 function createWindow() {
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 300, height: 300})
 
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
