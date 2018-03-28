203e697d9e6828ba8fe1b019e585918c91c889dd
Hiroshi Sugawara <5000164@users.noreply.github.com>
2018-03-29 08:44:17 +0900

    feat: 中央寄せで文字数が短い行が 1 文字だった場合に計算できるようにする

--- src/test/scala/jp/_5000164/text_emoji_generator/domain/TextTest.scala
+++ src/test/scala/jp/_5000164/text_emoji_generator/domain/TextTest.scala
@@ -3,7 +3,7 @@ package jp._5000164.text_emoji_generator.domain
 import org.scalatest.FreeSpec
 
 class TextTest extends FreeSpec {
-  "2 行で 4 文字の印字位置を計算" in {
+  "左寄せで 2 行で 4 文字の印字位置を計算" in {
     assert(Text.calculatePosition(List("よん", "もじ"), LeftJustified) == List(
       PrintChar("よ", 32.0, 32.0, 64.0),
       PrintChar("ん", 96.0, 32.0, 64.0),
@@ -12,13 +12,21 @@ class TextTest extends FreeSpec {
     ))
   }
 
-  "2 行で上が 2 文字で下が 3 文字の印字位置を計算" in {
+  "左寄せで 2 行で上が 2 文字で下が 3 文字の印字位置を計算" in {
     assert(Text.calculatePosition(List("した", "３文字"), LeftJustified) == List(
       PrintChar("し", 21.0, 32.0, 42.0),
       PrintChar("た", 63.0, 32.0, 42.0),
       PrintChar("３", 21.0, 96.0, 42.0),
       PrintChar("文", 63.0, 96.0, 42.0),
       PrintChar("字", 105.0, 96.0, 42.0)
+    ))
+  }
+
+  "中央寄せで 2 行で上が 2 文字で下が 1 文字の印字位置を計算" in {
+    assert(Text.calculatePosition(List("一二", "三"), Centering) == List(
+      PrintChar("一", 32.0, 32.0, 64.0),
+      PrintChar("二", 96.0, 32.0, 64.0),
+      PrintChar("三", 64.0, 96.0, 64.0)
     ))
   }
 
