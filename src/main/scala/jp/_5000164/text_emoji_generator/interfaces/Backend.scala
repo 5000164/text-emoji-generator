00eac07a2ba0f72109ed0c69661337056f314588
菅原 浩 <sncts35326@gmail.com>
2018-04-02 17:48:47 +0900

    refactor: 状態を増やした時に変更箇所を少なくするために現在の状態をまとめて渡す

--- src/main/scala/jp/_5000164/text_emoji_generator/interfaces/Backend.scala
+++ src/main/scala/jp/_5000164/text_emoji_generator/interfaces/Backend.scala
@@ -23,7 +23,7 @@ class Backend($: BackendScope[Unit, State]) {
             ^.id := "text",
             ^.value := state.text,
             ^.placeholder := "ここに入力",
-            ^.onChange ==> onChangeText(state.fontFace, state.align, f),
+            ^.onChange ==> onChangeText(state, f),
             Styles.text,
             if (state.fontFace == Gothic) ^.fontFamily := "Hiragino Kaku Gothic ProN" else ^.fontFamily := "Hiragino Mincho ProN",
             if (state.align == Left) ^.textAlign := "left" else ^.textAlign := "center"
@@ -36,15 +36,15 @@ class Backend($: BackendScope[Unit, State]) {
         ),
         <.div(
           Styles.selectColorWrapper,
-          <.input(^.value := state.color, ^.onChange ==> onChangeColor(state.text, state.fontFace, state.align), Styles.textColor),
-          <.button("色をランダムで選択", ^.onClick --> onClickRandomColor(state.text, state.fontFace, state.align, f), Styles.randomButton)
+          <.input(^.value := state.color, ^.onChange ==> onChangeColor(state), Styles.textColor),
+          <.button("色をランダムで選択", ^.onClick --> onClickRandomColor(state, f), Styles.randomButton)
         ),
         <.ul(
           Styles.colorList,
           Canvas.colorList.toVdomArray({
             case (key, value) => <.li(
               ^.key := key,
-              ^.onClick --> onClickColor(state.text, value, state.fontFace, state.align, f),
+              ^.onClick --> onClickColor(state, value, f),
               ^.style := js.Dictionary("backgroundColor" -> s"#$value").asInstanceOf[js.Object],
               Styles.colorListItem
             )
@@ -55,14 +55,14 @@ class Backend($: BackendScope[Unit, State]) {
           <.div("書体選択"),
           <.div(
             <.label(
-              <.input.radio(^.name := "type-face", ^.value := "gothic", ^.checked := state.fontFace == Gothic, ^.onChange ==> onClickFontFace(state.text, state.color, Gothic, state.align)),
+              <.input.radio(^.name := "type-face", ^.value := "gothic", ^.checked := state.fontFace == Gothic, ^.onChange ==> onClickFontFace(state, Gothic)),
               "ゴシック体",
               Styles.fontFaceButton
             )
           ),
           <.div(
             <.label(
-              <.input.radio(^.name := "type-face", ^.value := "mincho", ^.checked := state.fontFace == Mincho, ^.onChange ==> onClickFontFace(state.text, state.color, Mincho, state.align)),
+              <.input.radio(^.name := "type-face", ^.value := "mincho", ^.checked := state.fontFace == Mincho, ^.onChange ==> onClickFontFace(state, Mincho)),
               "明朝体",
               Styles.fontFaceButton
             )
@@ -73,14 +73,14 @@ class Backend($: BackendScope[Unit, State]) {
           <.div("位置選択"),
           <.div(
             <.label(
-              <.input.radio(^.name := "align", ^.value := "left", ^.checked := state.align == Left, ^.onChange ==> onClickAlign(state.text, state.color, state.fontFace, Left)),
+              <.input.radio(^.name := "align", ^.value := "left", ^.checked := state.align == Left, ^.onChange ==> onClickAlign(state, Left)),
               "左寄せ",
               Styles.alignButton
             )
           ),
           <.div(
             <.label(
-              <.input.radio(^.name := "align", ^.value := "center", ^.checked := state.align == Center, ^.onChange ==> onClickAlign(state.text, state.color, state.fontFace, Center)),
+              <.input.radio(^.name := "align", ^.value := "center", ^.checked := state.align == Center, ^.onChange ==> onClickAlign(state, Center)),
               "中央寄せ",
               Styles.alignButton
             )
@@ -90,52 +90,52 @@ class Backend($: BackendScope[Unit, State]) {
     )
   }
 
-  def onChangeText(fontFace: FontFace, align: Align, s: StateAccessPure[String])(e: ReactEventFromInput): Callback = {
+  def onChangeText(state: State, s: StateAccessPure[String])(e: ReactEventFromInput): Callback = {
     val updatedText = e.target.value
     val color = Random.shuffle(colorList).head._2
 
     {
       $.modState(_.copy(text = updatedText))
     } >> {
-      Canvas.generate(State(updatedText, color, fontFace, align))
+      Canvas.generate(State(updatedText, color, state.fontFace, state.align))
     } >> {
       s.setState(color)
     }
   }
 
-  def onChangeColor(text: String, fontFace: FontFace, align: Align)(e: ReactEventFromInput): Callback = {
+  def onChangeColor(state: State)(e: ReactEventFromInput): Callback = {
     val updatedColor = e.target.value
     $.modState(_.copy(color = updatedColor))
   } >> {
     val updatedColor = e.target.value
-    Canvas.generate(State(text, updatedColor, fontFace, align))
+    Canvas.generate(State(state.text, updatedColor, state.fontFace, state.align))
   }
 
-  def onClickColor(text: String, color: String, fontFace: FontFace, align: Align, s: StateAccessPure[String]): Callback = {
-    Canvas.generate(State(text, color, fontFace, align))
+  def onClickColor(state: State, updatedColor: String, s: StateAccessPure[String]): Callback = {
+    Canvas.generate(State(state.text, updatedColor, state.fontFace, state.align))
   } >> {
-    s.setState(color)
+    s.setState(updatedColor)
   }
 
-  def onClickRandomColor(text: String, fontFace: FontFace, align: Align, s: StateAccessPure[String]): Callback = {
+  def onClickRandomColor(state: State, s: StateAccessPure[String]): Callback = {
     val color = Random.shuffle(colorList).head._2
 
     {
-      Canvas.generate(State(text, color, fontFace, align))
+      Canvas.generate(State(state.text, color, state.fontFace, state.align))
     } >> {
       s.setState(color)
     }
   }
 
-  def onClickFontFace(text: String, color: String, fontFace: FontFace, align: Align)(e: ReactEventFromInput): Callback = {
+  def onClickFontFace(state: State, fontFace: FontFace)(e: ReactEventFromInput): Callback = {
     $.modState(_.copy(fontFace = fontFace))
   } >> {
-    Canvas.generate(State(text, color, fontFace, align))
+    Canvas.generate(State(state.text, state.color, fontFace, state.align))
   }
 
-  def onClickAlign(text: String, color: String, fontFace: FontFace, align: Align)(e: ReactEventFromInput): Callback = {
+  def onClickAlign(state: State, align: Align)(e: ReactEventFromInput): Callback = {
     $.modState(_.copy(align = align))
   } >> {
-    Canvas.generate(State(text, color, fontFace, align))
+    Canvas.generate(State(state.text, state.color, state.fontFace, align))
   }
 }
