ca64177d98bd3fbe379c1a89dc886d4b04b2559b
Hiroshi Sugawara <5000164@users.noreply.github.com>
2018-05-15 20:37:11 +0900

    refactor: 選択できる文字の色は外部とのやりとりのためのものではなく、文字自体が持つ情報なので interfaces から domain へ移動

--- src/main/scala/jp/_5000164/text_emoji_generator/interfaces/Canvas.scala
+++ src/main/scala/jp/_5000164/text_emoji_generator/interfaces/Canvas.scala
@@ -46,27 +46,4 @@ object Canvas {
     }
     dialog.showSaveDialog(null, option, callback)
   }
-
-  val colorList = List(
-    ("Red", "F44336"),
-    ("Pink", "E91E63"),
-    ("Purple", "9C27B0"),
-    ("Deep Purple", "673AB7"),
-    ("Indigo", "3F51B5"),
-    ("Blue", "2196F3"),
-    ("Light Blue", "03A9F4"),
-    ("Cyan", "00BCD4"),
-    ("Teal", "009688"),
-    ("Green", "4CAF50"),
-    ("Light Green", "8BC34A"),
-    ("Lime", "CDDC39"),
-    ("Yellow", "FFEB3B"),
-    ("Amber", "FFC107"),
-    ("Orange", "FF9800"),
-    ("Deep Orange", "FF5722"),
-    ("Brown", "795548"),
-    ("Grey", "9E9E9E"),
-    ("Blue Grey", "607D8B"),
-    ("Black", "000000")
-  )
 }
