12d17d327f00ea0f6b71075bd73313d308a407d7
菅原 浩 <sncts35326@gmail.com>
2018-05-20 20:48:39 +0900

    fix: List ではなく Seq を使うようにしたので修正

--- src/test/scala/jp/_5000164/text_emoji_generator/interfaces/CanvasSpec.scala
+++ src/test/scala/jp/_5000164/text_emoji_generator/interfaces/CanvasSpec.scala
@@ -7,7 +7,7 @@ class CanvasSpec extends FreeSpec {
   "左寄せで 2 行で 4 文字の印字位置を計算" in {
     val text = """よん
                  |もじ""".stripMargin
-    assert(Canvas.calculatePrintChar(text, Left) === List(
+    assert(Canvas.calculatePrintChar(text, Left) === Seq(
       PrintChar("よ", 32.0, 32.0, 64.0, 64.0),
       PrintChar("ん", 96.0, 32.0, 64.0, 64.0),
       PrintChar("も", 32.0, 96.0, 64.0, 64.0),
@@ -18,7 +18,7 @@ class CanvasSpec extends FreeSpec {
   "左寄せで 2 行で上が 2 文字で下が 3 文字の印字位置を計算" in {
     val text = """した
                  |３文字""".stripMargin
-    assert(Canvas.calculatePrintChar(text, Left) === List(
+    assert(Canvas.calculatePrintChar(text, Left) === Seq(
       PrintChar("し", 21.0, 32.0, 42.0, 64.0),
       PrintChar("た", 63.0, 32.0, 42.0, 64.0),
       PrintChar("３", 21.0, 96.0, 42.0, 64.0),
@@ -30,7 +30,7 @@ class CanvasSpec extends FreeSpec {
   "中央寄せで 2 行で上が 2 文字で下が 1 文字の印字位置を計算" in {
     val text = """一二
                  |三""".stripMargin
-    assert(Canvas.calculatePrintChar(text, Center) === List(
+    assert(Canvas.calculatePrintChar(text, Center) === Seq(
       PrintChar("一", 32.0, 32.0, 64.0, 64.0),
       PrintChar("二", 96.0, 32.0, 64.0, 64.0),
       PrintChar("三", 64.0, 96.0, 64.0, 64.0)
@@ -40,7 +40,7 @@ class CanvasSpec extends FreeSpec {
   "中央寄せで 2 行で上が 2 文字で下が 3 文字の印字位置を計算" in {
     val text = """一二
                  |三四五""".stripMargin
-    assert(Canvas.calculatePrintChar(text, Center) === List(
+    assert(Canvas.calculatePrintChar(text, Center) === Seq(
       PrintChar("一", 43.0, 32.0, 42.0, 64.0),
       PrintChar("二", 85.0, 32.0, 42.0, 64.0),
       PrintChar("三", 22.0, 96.0, 42.0, 64.0),
