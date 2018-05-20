6b528d5d4a9e0d6452dcc10fec45cd59eb2854cb
Hiroshi Sugawara <5000164@users.noreply.github.com>
2018-05-20 19:57:07 +0900

    refactor: 表示用のデータ構造に変換する処理は interface に関わるものなので interface 層に移譲

--- src/test/scala/jp/_5000164/text_emoji_generator/domain/TextTest.scala
+++ src/test/scala/jp/_5000164/text_emoji_generator/domain/TextTest.scala
@@ -1,13 +1,13 @@
 package jp._5000164.text_emoji_generator.domain
 
-import jp._5000164.text_emoji_generator.interfaces.PrintChar
+import jp._5000164.text_emoji_generator.interfaces.{Canvas, PrintChar}
 import org.scalatest.FreeSpec
 
 class TextTest extends FreeSpec {
   "左寄せで 2 行で 4 文字の印字位置を計算" in {
     val text = """よん
                  |もじ""".stripMargin
-    assert(Text.calculatePosition(text, Left) === List(
+    assert(Canvas.calculatePrintChar(text, Left) === List(
       PrintChar("よ", 32.0, 32.0, 64.0, 64.0),
       PrintChar("ん", 96.0, 32.0, 64.0, 64.0),
       PrintChar("も", 32.0, 96.0, 64.0, 64.0),
@@ -19,7 +19,7 @@ class TextTest extends FreeSpec {
   "左寄せで 2 行で上が 2 文字で下が 3 文字の印字位置を計算" in {
     val text = """した
                  |３文字""".stripMargin
-    assert(Text.calculatePosition(text, Left) === List(
+    assert(Canvas.calculatePrintChar(text, Left) === List(
       PrintChar("し", 21.0, 32.0, 42.0, 64.0),
       PrintChar("た", 63.0, 32.0, 42.0, 64.0),
       PrintChar("３", 21.0, 96.0, 42.0, 64.0),
@@ -31,7 +31,7 @@ class TextTest extends FreeSpec {
   "中央寄せで 2 行で上が 2 文字で下が 1 文字の印字位置を計算" in {
     val text = """一二
                  |三""".stripMargin
-    assert(Text.calculatePosition(text, Center) === List(
+    assert(Canvas.calculatePrintChar(text, Center) === List(
       PrintChar("一", 32.0, 32.0, 64.0, 64.0),
       PrintChar("二", 96.0, 32.0, 64.0, 64.0),
       PrintChar("三", 64.0, 96.0, 64.0, 64.0)
@@ -41,7 +41,7 @@ class TextTest extends FreeSpec {
   "中央寄せで 2 行で上が 2 文字で下が 3 文字の印字位置を計算" in {
     val text = """一二
                  |三四五""".stripMargin
-    assert(Text.calculatePosition(text, Center) === List(
+    assert(Canvas.calculatePrintChar(text, Center) === List(
       PrintChar("一", 43.0, 32.0, 42.0, 64.0),
       PrintChar("二", 85.0, 32.0, 42.0, 64.0),
       PrintChar("三", 22.0, 96.0, 42.0, 64.0),
