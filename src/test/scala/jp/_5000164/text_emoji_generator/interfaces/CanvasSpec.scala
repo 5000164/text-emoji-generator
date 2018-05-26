dcf38c3b923380d3fdc6b22398c6e4daecd99a70
菅原 浩 <sncts35326@gmail.com>
2018-05-27 01:36:01 +0900

    feat: テストの意図をより明確にするために再構築

--- src/test/scala/jp/_5000164/text_emoji_generator/interfaces/CanvasSpec.scala
+++ src/test/scala/jp/_5000164/text_emoji_generator/interfaces/CanvasSpec.scala
@@ -1,102 +1,106 @@
 package jp._5000164.text_emoji_generator.interfaces
 
 import jp._5000164.text_emoji_generator.domain.{Center, Left}
-import org.scalatest.FreeSpec
+import org.scalatest.FeatureSpec
 
-class CanvasSpec extends FreeSpec {
-  "左寄せで 2 行で 4 文字の印字位置を計算" in {
-    val text = """よん
-                 |もじ""".stripMargin
-    assert(Canvas.calculatePrintChar(text, Left) === Seq(
-      PrintChar("よ", 32.0, 32.0, 64.0, 64.0),
-      PrintChar("ん", 96.0, 32.0, 64.0, 64.0),
-      PrintChar("も", 32.0, 96.0, 64.0, 64.0),
-      PrintChar("じ", 96.0, 96.0, 64.0, 64.0)))
+class CanvasSpec extends FeatureSpec {
+  feature("左寄せの位置が計算できる") {
+    scenario("1 行目と 2 行目が同じ文字数") {
+      val text = """12
+                   |34""".stripMargin
+      assert(Canvas.calculatePrintChar(text, Left) === Seq(
+        PrintChar("1", 32.0, 32.0, 64.0, 64.0),
+        PrintChar("2", 96.0, 32.0, 64.0, 64.0),
+        PrintChar("3", 32.0, 96.0, 64.0, 64.0),
+        PrintChar("4", 96.0, 96.0, 64.0, 64.0)))
+    }
+
+    scenario("1 行目よりも 2 行目の方が文字数が多い") {
+      val text = """12
+                   |345""".stripMargin
+      assert(Canvas.calculatePrintChar(text, Left) === Seq(
+        PrintChar("1", 21.333333333333332, 32.0, 42.666666666666664, 64.0),
+        PrintChar("2", 64.0, 32.0, 42.666666666666664, 64.0),
+        PrintChar("3", 21.333333333333332, 96.0, 42.666666666666664, 64.0),
+        PrintChar("4", 64.0, 96.0, 42.666666666666664, 64.0),
+        PrintChar("5", 106.66666666666667, 96.0, 42.666666666666664, 64.0)))
+    }
   }
 
-  "左寄せで 2 行で上が 2 文字で下が 3 文字の印字位置を計算" in {
-    val text = """した
-                 |３文字""".stripMargin
-    assert(Canvas.calculatePrintChar(text, Left) === Seq(
-      PrintChar("し", 21.333333333333332, 32.0, 42.666666666666664, 64.0),
-      PrintChar("た", 64.0, 32.0, 42.666666666666664, 64.0),
-      PrintChar("３", 21.333333333333332, 96.0, 42.666666666666664, 64.0),
-      PrintChar("文", 64.0, 96.0, 42.666666666666664, 64.0),
-      PrintChar("字", 106.66666666666667, 96.0, 42.666666666666664, 64.0)))
+  feature("中央寄せの計算ができる") {
+    scenario("1 行目よりも 2 行目の方が文字数が少ない") {
+      val text = """12
+                   |3""".stripMargin
+      assert(Canvas.calculatePrintChar(text, Center) === Seq(
+        PrintChar("1", 32.0, 32.0, 64.0, 64.0),
+        PrintChar("2", 96.0, 32.0, 64.0, 64.0),
+        PrintChar("3", 64.0, 96.0, 64.0, 64.0)))
+    }
+
+    scenario("1 行目よりも 2 行目の方が文字数が多い") {
+      val text = """12
+                   |345""".stripMargin
+      assert(Canvas.calculatePrintChar(text, Center) === Seq(
+        PrintChar("1", 42.66666666666667, 32.0, 42.666666666666664, 64.0),
+        PrintChar("2", 85.33333333333333, 32.0, 42.666666666666664, 64.0),
+        PrintChar("3", 21.333333333333332, 96.0, 42.666666666666664, 64.0),
+        PrintChar("4", 64.0, 96.0, 42.666666666666664, 64.0),
+        PrintChar("5", 106.66666666666667, 96.0, 42.666666666666664, 64.0)))
+    }
   }
 
-  "左寄せで 3 行の印字位置を計算" in {
-    val text = """さ
-                 |ん
-                 |行""".stripMargin
-    assert(Canvas.calculatePrintChar(text, Left) === Seq(
-      PrintChar("さ", 21.333333333333332, 21.333333333333332, 42.666666666666664, 42.666666666666664),
-      PrintChar("ん", 21.333333333333332, 64.0, 42.666666666666664, 42.666666666666664),
-      PrintChar("行", 21.333333333333332, 106.66666666666667, 42.666666666666664, 42.666666666666664)))
+  feature("高さの計算ができる") {
+    scenario("3 行") {
+      val text = """1
+                   |2
+                   |3""".stripMargin
+      assert(Canvas.calculatePrintChar(text, Left) === Seq(
+        PrintChar("1", 21.333333333333332, 21.333333333333332, 42.666666666666664, 42.666666666666664),
+        PrintChar("2", 21.333333333333332, 64.0, 42.666666666666664, 42.666666666666664),
+        PrintChar("3", 21.333333333333332, 106.66666666666667, 42.666666666666664, 42.666666666666664)))
+    }
   }
 
-  "中央寄せで 2 行で上が 2 文字で下が 1 文字の印字位置を計算" in {
-    val text = """一二
-                 |三""".stripMargin
-    assert(Canvas.calculatePrintChar(text, Center) === Seq(
-      PrintChar("一", 32.0, 32.0, 64.0, 64.0),
-      PrintChar("二", 96.0, 32.0, 64.0, 64.0),
-      PrintChar("三", 64.0, 96.0, 64.0, 64.0)))
-  }
+  feature("[] で囲んだ部分は 1 文字分の幅になる") {
+    scenario("行の先頭で挟む") {
+      val text = "[12]3"
+      assert(Canvas.calculatePrintChar(text, Left) === Seq(
+        PrintChar("1", 16.0, 64.0, 32.0, 128.0),
+        PrintChar("2", 48.0, 64.0, 32.0, 128.0),
+        PrintChar("3", 96.0, 64.0, 64.0, 128.0)))
+    }
 
-  "中央寄せで 2 行で上が 2 文字で下が 3 文字の印字位置を計算" in {
-    val text = """一二
-                 |三四五""".stripMargin
-    assert(Canvas.calculatePrintChar(text, Center) === Seq(
-      PrintChar("一", 42.66666666666667, 32.0, 42.666666666666664, 64.0),
-      PrintChar("二", 85.33333333333333, 32.0, 42.666666666666664, 64.0),
-      PrintChar("三", 21.333333333333332, 96.0, 42.666666666666664, 64.0),
-      PrintChar("四", 64.0, 96.0, 42.666666666666664, 64.0),
-      PrintChar("五", 106.66666666666667, 96.0, 42.666666666666664, 64.0)))
-  }
+    scenario("行の末尾で挟む") {
+      val text = "1[23]"
+      assert(Canvas.calculatePrintChar(text, Left) === Seq(
+        PrintChar("1", 32.0, 64.0, 64.0, 128.0),
+        PrintChar("2", 80.0, 64.0, 32.0, 128.0),
+        PrintChar("3", 112.0, 64.0, 32.0, 128.0)))
+    }
 
-  "[]で囲んだ部分は 1 文字分の幅になる" in {
-    val text = """かこ
-                 |む[!!]""".stripMargin
-    assert(Canvas.calculatePrintChar(text, Left) === Seq(
-      PrintChar("か", 32.0, 32.0, 64.0, 64.0),
-      PrintChar("こ", 96.0, 32.0, 64.0, 64.0),
-      PrintChar("む", 32.0, 96.0, 64.0, 64.0),
-      PrintChar("!", 80.0, 96.0, 32.0, 64.0),
-      PrintChar("!", 112.0, 96.0, 32.0, 64.0)))
-  }
+    scenario("行の中間で挟む") {
+      val text = """1[23]4""".stripMargin
+      assert(Canvas.calculatePrintChar(text, Left) === Seq(
+        PrintChar("1", 21.333333333333332, 64.0, 42.666666666666664, 128.0),
+        PrintChar("2", 53.333333333333336, 64.0, 21.333333333333332, 128.0),
+        PrintChar("3", 74.66666666666666, 64.0, 21.333333333333332, 128.0),
+        PrintChar("4", 106.66666666666667, 64.0, 42.666666666666664, 128.0)))
+    }
 
-  "[]で囲んだ部分は 3 文字でも 1 文字分の幅になる" in {
-    val text = """かこ
-                 |む[!!!]""".stripMargin
-    assert(Canvas.calculatePrintChar(text, Left) === Seq(
-      PrintChar("か", 32.0, 32.0, 64.0, 64.0),
-      PrintChar("こ", 96.0, 32.0, 64.0, 64.0),
-      PrintChar("む", 32.0, 96.0, 64.0, 64.0),
-      PrintChar("!", 74.66666666666666, 96.0, 21.333333333333332, 64.0),
-      PrintChar("!", 95.99999999999999, 96.0, 21.333333333333332, 64.0),
-      PrintChar("!", 117.33333333333331, 96.0, 21.333333333333332, 64.0)))
-  }
+    scenario("挟む文字数を 3 文字にする") {
+      val text = """1[234]5""".stripMargin
+      assert(Canvas.calculatePrintChar(text, Left) === Seq(
+        PrintChar("1", 21.333333333333332, 64.0, 42.666666666666664, 128.0),
+        PrintChar("2", 49.77777777777777, 64.0, 14.222222222222221, 128.0),
+        PrintChar("3", 64.0, 64.0, 14.222222222222221, 128.0),
+        PrintChar("4", 78.22222222222223, 64.0, 14.222222222222221, 128.0),
+        PrintChar("5", 106.66666666666667, 64.0, 42.666666666666664, 128.0)))
+    }
 
-  "[]は行の先頭でも機能する" in {
-    val text = "[!!]!"
-    assert(Canvas.calculatePrintChar(text, Left) === Seq(
-      PrintChar("!", 16.0, 64.0, 32.0, 128.0),
-      PrintChar("!", 48.0, 64.0, 32.0, 128.0),
-      PrintChar("!", 96.0, 64.0, 64.0, 128.0)))
-  }
-
-  "[]は行の末尾でも機能する" in {
-    val text = "![!!]"
-    assert(Canvas.calculatePrintChar(text, Left) === Seq(
-      PrintChar("!", 32.0, 64.0, 64.0, 128.0),
-      PrintChar("!", 80.0, 64.0, 32.0, 128.0),
-      PrintChar("!", 112.0, 64.0, 32.0, 128.0)))
-  }
-
-  "[]の内容が空でも機能する" in {
-    val text = "![]"
-    assert(Canvas.calculatePrintChar(text, Left) === Seq(
-      PrintChar("!", 64.0, 64.0, 128.0, 128.0)))
+    scenario("挟む文字数が空") {
+      val text = "![]"
+      assert(Canvas.calculatePrintChar(text, Left) === Seq(
+        PrintChar("!", 64.0, 64.0, 128.0, 128.0)))
+    }
   }
 }
